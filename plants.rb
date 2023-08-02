6ccb74480f297bbc59fd2eb18b3a91330410f0d9
Heather McNeil <hd_mcneil@yahoo.com>
2023-08-02 15:33:06 -0600

    Modify plants.rb

--- plants.rb
+++ plants.rb
@@ -1,7 +1,7 @@
 gets_sunshine = true
-ounces_of_water = 10
+ounces_of_water = 6
 
-if ounces_of_water >= 10 && gets_sunshine
+if ounces_of_water >= 10 || gets_sunshine
     puts "Plant is thriving!"
 else 
     puts "Plant is dying!"
